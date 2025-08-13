75dbb29fc6677fd09015da6caf2a442c227c776e
Wulei26 <108564052+Wulei26@users.noreply.github.com>
2025-08-13 23:19:40 +0800

    20250813

---
+++ 3_单调栈/leetcode739.py
@@ -0,0 +1,21 @@
+from typing import List
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        n = len(temperatures)
+        ans = [0] * n
+        st = []
+        for i in range(n - 1, -1, -1):
+            t = temperatures[i]
+            while st and t >= temperatures[st[-1]]:
+                st.pop()
+            if st:
+                ans[i] = st[-1] - i
+            st.append(i)
+        return ans
+if __name__=='__main__':
+    a = Solution().dailyTemperatures([73,74,75,71,69,72,76,73])
+    print(a)
+    a = Solution().dailyTemperatures([73,74,75,71,69,72,76,73])
+    print(a)
+    a = Solution().dailyTemperatures([73,74,75,71,69,72,76,73])
+    print(a)
