fb056c4eebf980f16940e10a509b57c7e58c7e1a
wulei <wlei0928@163.com>
2025-08-21 10:03:46 +0800

    20250821

--- 6_图论算法/leetcode2192.py
+++ 6_图论算法/leetcode2192.py
@@ -1,27 +1,24 @@
 from typing import List
 from collections import deque
+
+
 class Solution:
-    def __init__(self) -> None:
-        self.path = []
-    # def BFS(self,graph:List[List[int]], isVisited: List[bool], node:int) -> None:
-
-
     def getAncestors(self, n: int, edges: List[List[int]]) -> List[List[int]]:
         ##既然是找祖先，那么将所有的单向边反过来，那么原来的起始点就变成了终点
         graph = [[] for _ in range(n)]
-        for x,y in edges:
+        for x, y in edges:
             graph[y].append(x)
         ##这构成了一个逆向的邻接表
         ## 那么从i出发，能够到达的所有点就是i的答案
         ans = [[] for _ in range(n)]
-        
+
         ## 使用广度优先遍历
-        
+
         for i in range(n):
             tmp = []
             isVisited = [False] * n
             isVisited[i] = True
-            q = deque([i]) ##从i出发
+            q = deque([i])  ##从i出发
             while q:
                 x = q.popleft()
                 for y in graph[x]:
@@ -33,12 +30,8 @@ class Solution:
         return ans
 
 
-if __name__=='__main__':
+if __name__ == "__main__":
     n = 8
-    edgeList = [[0,3],[0,4],[1,3],[2,4],[2,7],[3,5],[3,6],[3,7],[4,6]]
-    a = Solution().getAncestors(n,edgeList)
+    edgeList = [[0, 3], [0, 4], [1, 3], [2, 4], [2, 7], [3, 5], [3, 6], [3, 7], [4, 6]]
+    a = Solution().getAncestors(n, edgeList)
     print(a)
-
-
-
-        
